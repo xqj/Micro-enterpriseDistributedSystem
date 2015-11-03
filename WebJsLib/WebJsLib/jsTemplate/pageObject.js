a49b34057b5a661b1c2ab1197b7cf1177fbd4112
hsieh jing <xqj198404@gmail.com>
2015-11-03 17:34:15 +0800

    5

--- WebJsLib/WebJsLib/jsTemplate/pageObject.js
+++ WebJsLib/WebJsLib/jsTemplate/pageObject.js
@@ -5,7 +5,8 @@ var pageObject = {
     button:$("#buttonId"),
     base: pageBase,
     initPageObject: function () {
-        this.button.one("click", this.submit);
+        this.base.validateDefinition();
+        this.button.one("click", this.submit);//绑定点击事件
     },   
     setrequestId: function (requestId) {
         this.requestId = requestId;
@@ -21,12 +22,37 @@ var pageObject = {
             //关闭对话框清理函数
         });
     },
+    resetRules: function () {
+        //重置验证控件
+        pageObject.base.pageValidator.resetForm();
+        $("#inputid").rules("remove");
+    },
+    setSubmitRules: function () {
+        pageObject.resetRules();//在设置规则前清理规则
+        $("#mobile").rules("add", {//手机示例
+            required: true,
+            mobile:true,
+            minlength: 11,
+            maxlength: 11,
+            messages: {
+                required: "必填提示",
+                mobile:"手机格式提示",
+                minlength: "最小字符提示",
+                maxlength: "最大字符提示"
+            }
+        });
+    },
     submit: function () {
+        pageObject.setSubmitRules();//设置校验规则
+        if ($("#formid").valid()) {//进行校验
+            pageObject.button.one("click", pageObject.submit);//校验后绑定点击事件
+            return;
+        }
         pageObject.base.loadJsonFun("url", { parama: "parama" }, function (data) {
-            //逻辑处理
+            //逻辑处理,注意逻辑失败时可能需要重新绑定点击事件
         }, function () {
             //异常处理
-            pageObject.button.one("click", this.submit);
+            pageObject.button.one("click", pageObject.submit);//异常后绑定点击事件
         }, function () {
             //加载过场函数
         }, function () {
