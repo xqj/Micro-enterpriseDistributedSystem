3e790dffbde2da057455666d98627eba94b2d205
hsieh jing <xqj198404@gmail.com>
2015-11-23 23:30:16 +0800

    初始化configServer

---
+++ InformationModule/C#/mds/mds.ConfigService/ConfigServer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mds.ConfigService
+{
+    public class ConfigServer : IConfigServer
+    {
+        public BaseModel.SolutionConfiguration GetSolution(Guid solutionId, int version)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ InformationModule/C#/mds/mds.ConfigService/IConfigServer.cs
@@ -0,0 +1,20 @@
+﻿using mds.BaseModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mds.ConfigService
+{
+    public interface IConfigServer
+    {
+        /// <summary>
+        /// 获取解决方案配置
+        /// </summary>
+        /// <param name="solutionId"></param>
+        /// <param name="version"></param>
+        /// <returns></returns>
+        SolutionConfiguration GetSolution(Guid solutionId, int version);
+
+    }
+}
