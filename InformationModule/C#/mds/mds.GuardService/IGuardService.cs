70ec505d3bc71b4f5818a6a1d65603b3b194663b
hsieh jing <xqj198404@gmail.com>
2015-12-25 16:40:36 +0800

    增加组件活动图设计

--- InformationModule/C#/mds/mds.GuardService/IGuardService.cs
+++ InformationModule/C#/mds/mds.GuardService/IGuardService.cs
@@ -19,5 +19,23 @@ namespace mds.GuardService
        /// <param name="appIntanceID"></param>
        /// <returns></returns>
        OperationMessage NoticeAppIntanceReLoadConfig(int appIntanceID);
+       /// <summary>
+       /// 校验程序实例的文件完整性
+       /// </summary>
+       /// <param name="appIntanceID"></param>
+       /// <returns></returns>
+       OperationMessage VerifyAppFilse(int appIntanceID);
+       /// <summary>
+       /// 实例健康（功能完整、存活检查）测试
+       /// </summary>
+       /// <param name="appIntanceID"></param>
+       /// <returns></returns>
+       OperationMessage AppIntanceHealthyTest(int appIntanceID);
+       /// <summary>
+       /// 程序健康（功能完整、存活检查）测试
+       /// </summary>
+       /// <param name="appID"></param>
+       /// <returns></returns>
+       OperationMessage AppHealthyTest(int appID);
     }
 }
